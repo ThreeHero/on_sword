19b549b45a6fca47f6157d51e561e0b449c566e2
ThreeHero <2206563880@qq.com>
2024-07-28 21:11:05 +0800

    feat: useScroll

---
+++ src/hooks/useScroll.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+
+const useScroll = () => {
+  const [top, setTop] = useState(0)
+
+  const calculateScrollPercentage = () => {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0
+    const scrollHeight = Math.max(
+      document.body.scrollHeight,
+      document.documentElement.scrollHeight,
+      document.body.offsetHeight,
+      document.documentElement.offsetHeight,
+      document.body.clientHeight,
+      document.documentElement.clientHeight
+    )
+    const clientHeight = document.documentElement.clientHeight || window.innerHeight
+    const scrolled = Math.round((scrollTop / (scrollHeight - clientHeight)) * 100)
+    setTop(scrolled)
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', calculateScrollPercentage)
+    calculateScrollPercentage() // calculate once on mount
+
+    return () => {
+      window.removeEventListener('scroll', calculateScrollPercentage)
+    }
+  }, [])
+
+  return top
+}
+
+export default useScroll
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,5 @@
-import { MDEditor } from '@/components'
-import { Button } from 'antd'
-import { useState } from 'react'
-
 const Home = () => {
-  const [value, setValue] = useState<string>(`---
-theme: github
----  
-  
-  
-# Markdown-Navbar Demo
- 
-## Chicken Chicken
- 
-Chicken Chicken Chicken Chicken Chicken.
- 
-* Chicken Chicken Chicken Chicken Chicken.
-* Chicken Chicken Chicken Chicken Chicken.
-* Chicken Chicken Chicken Chicken Chicken.
- 
-### Chicken Chicken Chicken
- 
-Chicken Chicken Chicken Chicken Chicken.
- 
-#### Chicken Chicken Chicken Chicken
- 
-Chicken Chicken Chicken Chicken Chicken Chicken.`)
-
-  return (
-    <div>
-      <MDEditor
-        value={value}
-        onChange={setValue}
-      />
-    </div>
-  )
+  return <div>123</div>
 }
 
 export default Home
