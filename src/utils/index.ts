feb6b979851cec548b9e14451ac7b913e3e5d5f6
threehero <2206563880@qq.com>
2024-07-29 13:37:16 +0800

    feat: 重构工具库增强并修复打字机组件

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,9 @@
-export { default as http } from './service'
+import { baseURL } from '@/utils/service'
+
+export { default as http, baseURL } from './service'
 export * from './storage'
+
+// @ts-ignore
+String.prototype.resource = function () {
+  return baseURL + this
+}
