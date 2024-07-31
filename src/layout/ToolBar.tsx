7e29267c5abb8d99b4e3efa451bcc97a078ad53d
threehero <2206563880@qq.com>
2024-07-31 14:34:17 +0800

    feat: 重构登录页面增强用户体验并适配夜间模式

--- src/layout/ToolBar.tsx
+++ src/layout/ToolBar.tsx
@@ -1,6 +1,20 @@
+import { observer } from 'mobx-react'
 import styles from './index.less'
+import store from './store'
+import { MoonOutlined, SunOutlined } from '@ant-design/icons'
 
+const DarkIcon = observer(({ onClick }) => {
+  return store.isDark ? (
+    <MoonOutlined className={styles.icon} onClick={onClick} />
+  ) : (
+    <SunOutlined className={styles.icon} spin onClick={onClick} />
+  )
+})
 const ToolBar = () => {
-  return <div className={styles['tool-bar']}>ToolBar</div>
+  return (
+    <div className={styles['tool-bar']}>
+      <DarkIcon onClick={() => store.toggleDark()} />
+    </div>
+  )
 }
-export default ToolBar
+export default observer(ToolBar)
--- src/layout/store.ts
+++ src/layout/store.ts
@@ -1,8 +1,50 @@
+import { getCache, setCache } from '@/utils'
 import { makeAutoObservable } from 'mobx'
+import moment from 'moment'
+
+/**
+ * 判断时间是否在范围内
+ * @returns
+ */
+const isWithinTimeRange = (startTime: string, endTime: string): boolean => {
+  const currentMoment = moment('HH:mm:ss')
+  const startMoment = moment(startTime, 'HH:mm:ss')
+  const endMoment = moment(endTime, 'HH:mm:ss')
+  return currentMoment.isSameOrAfter(startMoment) && currentMoment.isSameOrBefore(endMoment)
+}
 
 class Store {
+  isDark = false
+
   constructor() {
     makeAutoObservable(this)
+    setTimeout(() => {
+      if (getCache('isDark', false) != null) {
+        this.toggleDark(getCache('isDark', false))
+      } else {
+        const isWith = isWithinTimeRange('09:00:00', '20:00:00')
+        this.toggleDark(!isWith)
+      }
+    }, 0)
+  }
+
+  /**
+   * 切换主题
+   */
+  toggleDark = (current?: boolean) => {
+    this.isDark = current ?? !this.isDark
+    const root = document.documentElement
+    const get = (key: string) => getComputedStyle(root).getPropertyValue(key)
+    const set = (key: string, getKey: string) => root.style.setProperty(key, get(getKey))
+    if (this.isDark) {
+      set('--current-bg', '--bg-color2')
+      set('--current-color', '--text-color2')
+    } else {
+      set('--current-bg', '--bg-color')
+      set('--current-color', '--text-color')
+    }
+
+    setCache('isDark', this.isDark, false)
   }
 }
 
