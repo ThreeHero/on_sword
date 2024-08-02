c602c2fab0e9a66c96ea215f2248a5391e652b26
threehero <2206563880@qq.com>
2024-08-02 17:07:40 +0800

    feat: editor

--- src/pages/editor/components/Editor.tsx
+++ src/pages/editor/components/Editor.tsx
@@ -1,11 +1,25 @@
 import { observer } from 'mobx-react'
 import { MDEditor } from '@/components'
 import { Form } from 'antd'
+import { FC, useState } from 'react'
+
+interface IProps {
+  value?: string
+  onChange?: (v: string) => void
+}
+const FormEditor: FC<IProps> = ({ value, onChange }) => {
+  const [_, refresh] = useState(false)
+  const handleChange = (v: string) => {
+    refresh(f => !f)
+    onChange(v)
+  }
+  return <MDEditor value={value ?? ''} onChange={handleChange} />
+}
 
 const Editor = ({ store }) => {
   return (
     <Form.Item name="content">
-      <MDEditor />
+      <FormEditor />
     </Form.Item>
   )
 }
